187b1942b758b626f08be856c22bec87a84b62de
Akash Sharma <aakash6263264@gmail.com>
2024-03-29 22:15:37 +0530

    fixing issues

--- src/components/StudentUser/StudentUser.jsx
+++ src/components/StudentUser/StudentUser.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./StudentUser.css";
 const StudentUser = () => {
   const navigate = useNavigate();
